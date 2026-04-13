7767a8c24fa85d5fb2d09eafe28781db2eda5ecc
ghoul789456 <3302927909@qq.com>
2026-04-13 17:56:20 +0800

    '侧边栏修改'

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
-import { useState,useContext } from 'react'
+import { useState, useContext } from 'react'
 import { Moon, Sun } from "@gravity-ui/icons";
-import { Switch } from "@heroui/react";
+import { Switch, Avatar, Input } from "@heroui/react";
 import ThemeContext from '../../contexts/ThemeContext';
 import defaultImg from '../../assets/default.jpg';
 import './index.css'
 export default function Header() {
-    const themes=useContext(ThemeContext)
-    if(!themes) throw new Error("no theme");
-    const {isDark,toggleTheme}=themes
-    
+    const themes = useContext(ThemeContext)
+    if (!themes) throw new Error("no theme");
+    const { isDark, toggleTheme } = themes
+
     return (
         <div className='header'>
             <div className='logo-img'>
                 <img src="" alt="logo" />
             </div>
+            <div><Input aria-label="Name" className="w-64" placeholder="想播放什么？" /></div>
             <div className='setting'>
-                <img src={defaultImg} alt="个人中心" />
+                {/* <img src={defaultImg} alt="个人中心" /> */}
+                <Avatar className='avatar-box'>
+                    <Avatar.Image  className='avatar-img' alt="John Doe" src={defaultImg} />
+                    <Avatar.Fallback>JD</Avatar.Fallback>
+                </Avatar>
+
+
                 <Switch
                     isSelected={isDark} onChange={toggleTheme}
                     size="lg"
--- client/src/views/home/index.tsx
+++ client/src/views/home/index.tsx
@@ -1,7 +1,33 @@
 import React from 'react'
-
+import './index.css'
 export default function Home() {
   return (
-    <div>home</div>
+    <div className='mian-box'>
+
+      <div className='list-item'>
+        <p>热播歌曲</p>
+        <div>
+          列表
+        </div>
+      </div>
+      <div className='list-item'>
+        <p>当红歌手</p>
+        <div>
+          列表
+        </div>
+      </div>
+      <div className='list-item'>
+        <p>热播歌曲</p>
+        <div>
+          列表
+        </div>
+      </div>
+      <div className='list-item'>
+        <p>热播歌曲</p>
+        <div>
+          列表
+        </div>
+      </div>
+    </div>
   )
 }
